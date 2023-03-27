a0cd5f266d0d808471b4f78ce332860737448d34
tiki <thiagoqua16@gmail.com>
2023-03-27 01:35:20 -0300

    Restructurado de código y agregado de barra de contacto.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
+import { ProyectsComponent } from './proyects/proyects.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProyectsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/proyects/proyects.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-proyects',
+  templateUrl: './proyects.component.html',
+  styleUrls: ['./proyects.component.css']
+})
+export class ProyectsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {}
+
+}
